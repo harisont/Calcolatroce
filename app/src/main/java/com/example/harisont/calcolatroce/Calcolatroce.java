e908f4811a1e1909c925a84c9d2521585437b9b6
Arianna Masciolini <uzkamascio@gmail.com>
2018-03-29 13:55:12 +0200

    Stupid layouts.

---
+++ app/src/main/java/com/example/harisont/calcolatroce/Calcolatroce.java
@@ -0,0 +1,13 @@
+package com.example.harisont.calcolatroce;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Calcolatroce extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calcolatroce);
+    }
+}
